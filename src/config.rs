6d2083fbf3d18e309f0169df3617d12a8bfc7408
zjmuye <107024249+zjmuye@users.noreply.github.com>
2025-05-27 23:41:30 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["op.ns.lv.eu.org"];
-pub const RS_PUB_KEY: &str = "faDOOI3h5AkEh1OolLBFwbtnfokBKVqXmA4xM12QI+U=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["xxx.org"];
+pub const RS_PUB_KEY: &str = "xxx=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
